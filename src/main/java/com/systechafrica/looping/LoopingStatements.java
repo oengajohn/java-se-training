8da98c20a7226212c3434a98e55b042534490ef3
oengajohn <johnongwae4@gmail.com>
2023-08-25 08:08:16 +0300

    While loop statement

--- src/main/java/com/systechafrica/looping/LoopingStatements.java
+++ src/main/java/com/systechafrica/looping/LoopingStatements.java
@@ -5,16 +5,32 @@ import java.util.logging.Logger;
 public class LoopingStatements {
     private static final Logger LOGGER = Logger.getLogger(LoopingStatements.class.getName());
 
-    public void forLoop(){
+    public void forLoop() {
         // initialize condition update
-        for( int counter = 0;  counter <= 10;  counter = counter + 1 )        {
-            LOGGER.info("Count = "+ counter);
+        for (int counter = 0; counter <= 10; counter = counter + 1) {
+            LOGGER.info("Count = " + counter);
         }
+        for (int counter = 0; counter <= 10; counter += 1) {
+            LOGGER.info("Count = " + counter);
+        }
+        for (int counter = 0; counter <= 10; counter++) {
+            LOGGER.info("Count = " + counter);
+        }
+    }
+
+    public void whileLoop() {
+        int studentCount = 0;
+        while (studentCount < 10) {
+            LOGGER.info("Student Count = " + studentCount);
+            studentCount++; // studentCount = studentCount + 1
+        }
+
     }
 
     public static void main(String[] args) {
         LoopingStatements app = new LoopingStatements();
-        app.forLoop();
+        // app.forLoop();
+        app.whileLoop();
 
     }
 }
