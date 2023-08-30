5cf51c43b68bbf661036420d52bb4aa668beee37
oengajohn <johnongwae4@gmail.com>
2023-08-30 08:01:03 +0300

    basic user input

---
+++ src/main/java/com/systechafrica/userinput/UserInputUsingScanner.java
@@ -0,0 +1,39 @@
+package com.systechafrica.userinput;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class UserInputUsingScanner {
+    private static final Logger LOGGER = Logger.getLogger(UserInputUsingScanner.class.getName());
+
+
+
+    public static void main(String[] args) {
+        final String DB_USERNAME = "mhusika";
+        final String DB_PASSWORD = "Admin123";
+
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Enter your username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        if ( username.equals(DB_USERNAME) && password.equals(DB_PASSWORD)){
+            System.out.println("Welcome to our website");
+        }
+        else{
+            System.err.println("Incorrect username or password");
+        }
+
+        scanner.close();
+
+
+
+
+
+
+    }
+
+}
